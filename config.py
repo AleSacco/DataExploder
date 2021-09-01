361a960c76f614df3c9744424774d56fe09f2e82
Alessio Sacco <85286496+AleSacco@users.noreply.github.com>
2021-09-01 15:27:50 +0200

    Add files via upload
    Optional config.py file (will be created by script anyway if not present)

---
+++ config.py
@@ -0,0 +1,8 @@
+config = {
+    "k": 1.96,
+    "Measurements file name": "data.csv",
+    "Uncertainties file name": "uncertainties.csv",
+    "Destination file name": "Exploded data.csv",
+    "Number of samples": 1000.0,
+    "Number of label columns": 3
+}
